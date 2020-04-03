8e242c5b610509de0efa6cfff431b6a82725d0a6
Zakir Hossen <web.zakirbd@gmail.com>
2020-04-03 08:51:45 +0600

    vue owl carousel plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    { src: '~/plugins/fontawesome.js', mode: 'client' }
+    { src: '~/plugins/slider.js', mode: 'client' }
   ],
   /*
   ** Nuxt.js dev-modules
