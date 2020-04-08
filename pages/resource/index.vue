99b75950feeb260bf357aa87ae8a5cb64091291e
Zakir Hossen <web.zakirbd@gmail.com>
2020-04-08 13:51:20 +0600

    resource page complete

--- pages/resource/index.vue
+++ pages/resource/index.vue
@@ -145,7 +145,7 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="heading">
-                            <h3> CSS5 Resources </h3>
+                            <h3> CSS3 Resources </h3>
                         </div>
                     </div>
                 </div>
@@ -394,6 +394,296 @@
                             </div>
                             <a href="https://www.webdesignrankings.com/resources/lolcolors/" class="title">Lol Colors</a>
                             <p>Curated color palettes inspiration. Another source for great color palettes.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="resource section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading">
+                            <h3> Image and Graphics</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row resource-list">
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/unsplash.svg" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://unsplash.com" class="title">Unsplash</a>
+                            <p>My #1 resource for free high-resolution photos. 10 new photos every 10 days.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/burst-logo.jpg" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://burst.shopify.com" class="title">Shopify Brust</a>
+                            <p>Browse thousands of beautiful copyright-free images. All our pictures are free to download for personal and commercial use, no attribution required.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/subtlepatterns.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="http://subtlepatterns.com/" class="title">Subtle Patterns</a>
+                            <p>High quality resource of tilable textured patterns, completely free to use.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/placeit.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://placeit.net/" class="title">PlaceIt</a>
+                            <p>Instant iPhone and Macbook mockups. Just place your screenshot on a device, no Photoshop.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/canva.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://placeit.net/" class="title">Canva</a>
+                            <p>Easily create beautiful designs with drag-and-drop features and professional layouts.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="resource section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading">
+                            <h3> Icons and Webfonts</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row resource-list">
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/fontawesome.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://fontawesome.com" class="title">Font Awesome</a>
+                            <p>My #1 resource for free high-resolution photos. 10 new photos every 10 days.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/iconfont.svg" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://icofont.com" class="title">Ico Font</a>
+                            <p>IcoFont is an icon font application that contains 2100+ icons in a single font. Download the font for free or build icons bundle and download custom build font.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/bootstrap.svg" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://icons.getbootstrap.com" class="title">Bootstrap Icon</a>
+                            <p>Official open source SVG icon library for Bootstrap.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/flaticon.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://www.flaticon.com" class="title">Flaticon</a>
+                            <p>The largest database of free icons available in PNG, SVG, EPS and PSD. It's even searchable.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/placeit.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://placeit.net/" class="title">PlaceIt</a>
+                            <p>Instant iPhone and Macbook mockups. Just place your screenshot on a device, no Photoshop.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="resource section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading">
+                            <h3> Design Inspiration </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row resource-list">
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/dribbble.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="#" class="title"> Dribbble </a>
+                            <p>The ultimate design inspiration resource, not just for web design. The show and tell for designers.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/landbook.jpg" alt="" class="img-fluid">
+                            </div>
+                            <a href="#" class="title"> Land Book </a>
+                            <p>Product landing page gallery. My #1 inspiration resource when I build a landing page.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/onepagelove.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="#" class="title">One Page Love</a>
+                            <p>The ultimate showcase of delicious One Page websites.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="resource section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading">
+                            <h3> Code Inspiration and Communities </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row resource-list">
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/codepen.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="#" class="title">Codepen</a>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad omnis voluptatum.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/codrops.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="http://tympanus.net/codrops/" class="title">Codrops</a>
+                            <p>Another blog, writing articles and tutorials about latest web trends, techniques and new possibilities.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/smashingmagazine.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="http://tympanus.net/codrops/" class="title">Smashing Magazine</a>
+                            <p>A very popular web design and development blog, writing about all things coding and designing.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/stackoverflow.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="#" class="title">Stack Overflow</a>
+                            <p>The biggest programmer community. Ask and find answers to HTML, CSS and JS questions.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/github.webp" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://github.com/" class="title">Github</a>
+                            <p>GitHub brings together the world's largest community of developers to discover, share, and build better software. From open source projects to private team</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/devto.svg" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://dev.to/" class="title">Dev.to</a>
+                            <p>Where programmers share ideas and help each other grow—A constructive and inclusive social network.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="resource section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading">
+                            <h3> PLANNING, TESTING, OPTIMIZATION AND DEPLOYMENT </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row resource-list">
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/w3c.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://validator.w3.org/" class="title">HTML Validator by W3C</a>
+                            <p>Check if your HTML markup is valid and contains no errors. Official W3C tool.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/w3c.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://jigsaw.w3.org/css-validator/" class="title">CSS Validator by W3C</a>
+                            <p>Check if your CSS code is valid and contains no errors. Official W3C tool.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/tinypng.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://tinypng.com/" class="title">Tinypng</a>
+                            <p>Make your website faster and save bandwidth. TinyPNG optimizes your PNG images by 50-80% while preserving full transparency!</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/responsinator.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://www.responsinator.com/" class="title">Responsinator</a>
+                            <p>The Responsinator is designed to test responsive websites on different device resolutions.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/googledev.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://developers.google.com/speed/pagespeed/insights/" class="title">Google PageSpeed Insights</a>
+                            <p>Quick tool to test your webpage for performance on all devices.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="block">
+                            <div class="icon">
+                                <img src="../../static/images/resources/git.png" alt="" class="img-fluid">
+                            </div>
+                            <a href="https://git-scm.com/" class="title">Git - Version Control System</a>
+                            <p>Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.</p>
                         </div>
                     </div>
                 </div>
