b3b7a15564c8337b5bf86cdc6efe20f24f120995
Zakir Hossen <web.zakirbd@gmail.com>
2020-04-13 01:37:21 +0600

    website feature image added for social media share

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:image', property: 'og:image', content: "/images/banner3.jpg" }
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/images/favicon.png' }
