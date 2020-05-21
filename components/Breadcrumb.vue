6add0d6d55076ea021874b0ac56cc052b03e2217
Zakir Hossen <web.zakirbd@gmail.com>
2020-05-22 01:51:41 +0600

    dynamic title added on breadcrumb

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="heading">
-                        <h2>Product Title Goes Here</h2>
+                        <h2>{{ title }}</h2>
                         <div class="index">
                             <router-link :to="{ name: 'index'}">Home</router-link> <font-awesome-icon :icon="['fas', 'chevron-right']" />
                             <router-link :to="{ name: 'product-slug', params: {slug: slug }}"> {{ name }} </router-link>
@@ -18,7 +18,7 @@
 
 <script>
 export default {
-    props: ['name', 'slug'],
+    props: ['name', 'slug', 'title'],
 }
 </script>
 
